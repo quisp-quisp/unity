0f7f3ee22e8c7a5f4063f904170971d4f3a38b1d
quisp-quisp <74887403+quisp-quisp@users.noreply.github.com>
2020-11-26 17:50:54 -0800

    Add files via upload

---
+++ Interactable.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] private float radius = 3f;
+
+    private GameObject player;
+    private UIManager uiManager;
+    private bool isInteractable;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<MoveController>().gameObject;
+        uiManager = FindObjectOfType<UIManager>().GetComponent<UIManager>();
+        SetInteractable(false);
+    }
+
+    private void Update()
+    {
+        // inside radius
+        if (Vector3.Distance(transform.position, player.transform.position) <= radius)
+        {
+            // inside viewport
+            Vector3 viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
+            if (viewportPosition.z >= 0 && 0 <= viewportPosition.x && viewportPosition.x <= 1 && 0 <= viewportPosition.y && viewportPosition.y <= 1)
+            {
+                Debug.Log("interactable");
+                uiManager.ShowInteractButton();
+                SetInteractable(true);
+            }
+            else
+            {
+                uiManager.HideInteractButton();
+                SetInteractable(false);
+            }
+        }
+
+        // adsf
+        //if (Input.GetButton("e"))
+        //{
+        //    if (isInteractable)
+        //    {
+        //        Debug.Log("interact");
+        //    }
+        //}
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    private void SetInteractable(bool isInteractable)
+    {
+        this.isInteractable = isInteractable;
+    }
+}
---
+++ UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private GameObject interactButton;
+
+    public void ShowInteractButton()
+    {
+        interactButton.SetActive(true);
+    }
+
+    public void HideInteractButton()
+    {
+        interactButton.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        interactButton = GameObject.Find("InteractButton");
+        HideInteractButton();
+    }
+}
